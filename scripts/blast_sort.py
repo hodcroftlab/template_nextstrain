6012243f64290d75463619e28d7888c0cffec54f
Nadia Neuner-Jehle <nadia.neunerjehle@gmail.com>
2025-03-14 09:28:54 +0100

    fix: correct indentation in blast_sort.py for improved readability; update lat & longs

--- scripts/blast_sort.py
+++ scripts/blast_sort.py
@@ -30,20 +30,20 @@ def main():
     selected_seqs = []
 
     # Process sequences based on the specified length range
-   if args.range != "whole_genome":
-        blast_results = blast_results[(blast_results.diff_length_ref >= length_range[0]) & (blast_results.diff_length_ref <= length_range[1])]
-        
-        for seq_record in sequences:
-            if seq_record.id in blast_results.qseqid.unique():
-                # Get the relevant BLAST hit
-                blast_hit = blast_results[blast_results.qseqid == seq_record.id].iloc[0]
-                # Extract the aligned region
-                reg_seq = seq_record.seq[blast_hit["qstart"] - 1:blast_hit["qend"]]
-                # Create new sequence record
-                new_seq_record = seq_record[:0]  # Copy metadata
-                new_seq_record.seq = reg_seq
-                if length_range[0] <= len(reg_seq) <= length_range[1]:
-                    selected_seqs.append(new_seq_record)
+    if args.range != "whole_genome":
+            blast_results = blast_results[(blast_results.diff_length_ref >= length_range[0]) & (blast_results.diff_length_ref <= length_range[1])]
+            
+            for seq_record in sequences:
+                if seq_record.id in blast_results.qseqid.unique():
+                    # Get the relevant BLAST hit
+                    blast_hit = blast_results[blast_results.qseqid == seq_record.id].iloc[0]
+                    # Extract the aligned region
+                    reg_seq = seq_record.seq[blast_hit["qstart"] - 1:blast_hit["qend"]]
+                    # Create new sequence record
+                    new_seq_record = seq_record[:0]  # Copy metadata
+                    new_seq_record.seq = reg_seq
+                    if length_range[0] <= len(reg_seq) <= length_range[1]:
+                        selected_seqs.append(new_seq_record)
     else:
         for seq_record in sequences:
             seq_length = len(seq_record.seq)
@@ -54,5 +54,5 @@ def main():
     SeqIO.write(selected_seqs, args.out_seqs, "fasta")
     print(f"Filtering retained {len(selected_seqs)} out of {len(sequences)} sequences in this file")
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
